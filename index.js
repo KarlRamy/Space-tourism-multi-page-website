fbacc7ed861d4bd9b627f70690ff9f2e63688b81
Karl Ramy <karlramysaad15@gmail.com>
2026-08-08 19:40:27 +0300

    Refactor planet and crew data structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const planets = {
     moon: {
         name: "Moon",
-        image: "destination/image-moon.png",
+        image: "image-moon.png",
         info: "The Moon is the earths only sattellite",
         distance: "384 TH. KM",
         time: "3 DAYS"
@@ -9,7 +9,7 @@ const planets = {
 
     mars: {
         name: "Mars",
-        image: "destination/image-mars.png",
+        image: "image-mars.png",
         info: "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
          distance: "225 MIL. KM",
         time: "9 MONTHS"
@@ -17,7 +17,7 @@ const planets = {
     
     europa: {
         name: "Europa",
-        image: "destination/image-europa.png",
+        image: "image-europa.png",
         info: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin",
         distance: "628 MIL. KM",
         time: "3 YEARS"
@@ -26,7 +26,7 @@ const planets = {
 
     titan: {
         name: "Titan",
-        image: "destination/image-titan.png",
+        image: "image-titan.png",
         info: "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
         distance: "1.6 BIL. KM",
         time: "7 YEARS"
@@ -52,25 +52,25 @@ if (document.getElementById("planet-img")) {
 
 const crew = [
     {
-        image: "crew/image-anousheh-ansari.png",
+        image: "image-anousheh-ansari.png",
         role: "FLIGHT ENGINEER",
         name: "Anousheh Ansari",
         bio: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space."
     },
     {
-        image: "crew/image-douglas-hurley.png",
+        image: "image-douglas-hurley.png",
         role: "Commander",
         name: "Douglas Hurley",
         bio: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2."
     },
     {
-        image: "crew/image-mark-shuttleworth.png",
+        image: "image-mark-shuttleworth.png",
         role: "Mission Specialist",
         name: "Mark Shuttleworth",
         bio: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist."
     },
     {
-        image: "crew/image-victor-glover.png",
+        image: "image-victor-glover.png",
         role: "Pilot",
         name: "Victor Glover",
         bio: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18. He was a crew member of Expedition 64, and served as a station systems flight engineer."
@@ -103,21 +103,21 @@ const tech = [
         p: "The terminology",
         terminology: "Spaceport",
         pp: " A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
-        image: "technology/image-spaceport-portrait.jpg"
+        image: "image-spaceport-portrait.jpg"
     },
 
     {
         p: "The terminology",
         terminology: "Space capsule",
         pp: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
-        image: "technology/image-space-capsule-portrait.jpg"
+        image: "image-space-capsule-portrait.jpg"
     },
 
     {
         p: "The terminology",
         terminology: "Launch vehicle",
         pp: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
-        image: "technology/image-launch-vehicle-portrait.jpg"
+        image: "image-launch-vehicle-portrait.jpg"
     }
 ]
 
